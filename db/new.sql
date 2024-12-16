e0f203b8d13b83be70ba24c7b4325b3533cb0d32
Najibullah Jafari <najib2020202020@gmail.com>
2024-12-16 14:53:30 +0430

    add new sql

--- db/new.sql
+++ db/new.sql
@@ -22,3 +22,16 @@ CREATE TABLE profits (
 ALTER TABLE api_credentials
 ADD COLUMN my_loan DECIMAL(15, 2) DEFAULT 0 AFTER base_url,
 ADD COLUMN my_money DECIMAL(15, 2) DEFAULT 0 AFTER my_loan;
+
+CREATE TABLE `loans` (
+    `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `dealer_code` VARCHAR(64) NOT NULL,
+    `bank_id` INT(10) UNSIGNED NOT NULL,
+    `loan_amount` DECIMAL(10, 2) NOT NULL,
+    `loan_term` INT(10) NOT NULL,
+    `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`bank_id`) REFERENCES `bank` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = UTF8MB4_GENERAL_CI;
+
+product
