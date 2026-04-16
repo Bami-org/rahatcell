5d7f9080183cf4693934678b5abe63d24e19987f
Najibullah Jafari <najib2020202020@gmail.com>
2026-04-16 13:43:00 +0430

    Add new tables for product, currency, and balance; update customer table with customer_type

--- db/new.sql
+++ db/new.sql
@@ -1,7 +1,58 @@
 -- najib dev
 ALTER TABLE customer
 ADD COLUMN customer_type ENUM('wholesale', 'retail') NOT NULL;
+CREATE TABLE `product` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `amount` int(11) NOT NULL,
+  `unit_id` int(10) unsigned NOT NULL,
+  `category_id` int(10) unsigned NOT NULL,
+  `sub_category_id` int(10) unsigned NOT NULL,
+  `dollar_buy_price` decimal(10,2) NOT NULL,
+  `dollar_sale_price` decimal(10,2) NOT NULL,
+  `toman_buy_price` float NOT NULL,
+  `toman_sale_price` float NOT NULL,
+  `lyra_buy_price` float NOT NULL,
+  `lyra_sale_price` float NOT NULL,
+  `euro_buy_price` float NOT NULL,
+  `euro_sale_price` float NOT NULL,
+  `description` text NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `currency` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `symbol` varchar(32) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO currency (id,name,symbol,created) VALUES
+(1,'تومن','IR','2024-02-24 00:00:00'),
+(2,'دالر','$','2024-02-24 00:00:00'),
+(4,'لیر','TL','2024-02-24 00:00:00'),
+(5,'یورو','ER','2024-02-26 00:00:00');
+CREATE TABLE `balance` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` int(10) unsigned NOT NULL,
+  `balance` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `balance_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO balance (id,customer_id,balance,description,created,updated) VALUES
+(1,3,5000,'خدمات آنلاین نعمان کوهستانی','2024-02-25 18:55:51','2024-03-02 19:08:32'),
+(4,4,30000,'تست بیلانس','2024-02-27 12:50:57','2024-03-02 19:08:32'),
+(5,1,1500,'500 به علاوه یک هزار اضافه شد','2024-02-29 21:55:31','2024-03-04 04:18:39'),
+(8,8,800,'300 دیگر به علاوه 500 اضافه شد','2024-03-04 16:23:11','2024-03-04 16:23:11');
+SET FOREIGN_KEY_CHECKS=1;
 CREATE TABLE profits (
     id INT AUTO_INCREMENT PRIMARY KEY,
     profit_toman DECIMAL(15, 2) NOT NULL,
